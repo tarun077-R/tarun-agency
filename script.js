9563268ed9ac0c41840f185497a9f6c488435248
tarun077-R <tarunrawat8350@gmail.com>
2025-02-24 11:38:34 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 tl.from(".line h1", {
     duration: 0.6,
     y: 150,
@@ -40,3 +41,25 @@ tl.from("#page1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+y:120,
+stagger:0.2
+})
+tl.from("#nav",{
+opacity:0,
+duration:0.6
+})
+}
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation()
+cursorAnimation()
